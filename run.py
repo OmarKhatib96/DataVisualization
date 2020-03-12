ff1a6be7583ca06bb035cf80a4e598b3b7ec6252
Omar <khatibomar49@gmail.com>
2020-03-13 00:34:01 +0100

    SOme improvement on the layout

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 
 from network_visualization import network_visualization
 
-
-net_vis=network_visualization()
+net_vis=network_visualization("Data visualization with 2 layers"," Wei-Ting Sample","Datavisualization.html")
 net_vis.network_representation()
 
 
